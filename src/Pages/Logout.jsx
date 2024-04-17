8cbb00671c81a9d06b3115c236772a1f1ef25e51
sriramk1n1 <sriramjkini@gmail.com>
2024-04-17 15:54:52 +0530

    fix

--- src/Pages/Logout.jsx
+++ src/Pages/Logout.jsx
@@ -1,9 +1,14 @@
 import Cookies from 'js-cookie'
+import { useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 
 function Logout(props) {
-    Cookies.remove('accessToken')
-    props.setlogin(false)
+    useEffect(
+        ()=>{
+            Cookies.remove("accessToken")
+            props.setlogin(false),[]
+        }
+    );
     return <Navigate to="/" replace />;
   }
 
